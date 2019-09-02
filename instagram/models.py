a9d3ed21f1e355d4552aa6cfa6f4573b1e4ce5a3
limooh <limobrian290@gmail.com>
2019-09-02 08:22:19 +0300

    Attribute Variables for Profile class to represent different columns in database

--- instagram/models.py
+++ instagram/models.py
@@ -8,3 +8,10 @@ import datetime
 # Create your models here.
 
 class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=350) 
+    profile_pic = models.ImageField(upload_to='ProfilePicture/')
+    profile_avatar = models.ImageField(upload_to='AvatorPicture/')
+    date = models.DateTimeField(auto_now_add=True, null= True)
