58b659e40e03ace95e29351aaa279596912d50eb
limooh <limobrian290@gmail.com>
2019-09-02 07:34:45 +0300

    home page view function

--- instagram/views.py
+++ instagram/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    all_images = Image.objects.all()
+    all_users = Profile.objects.all()
+    next = request.GET.get('next')
+    if next: return redirect(next)
+    return render(request, 'display/home.html',  {"all_images": all_images}, {"all_users":all_users})
