a6908c9858b1affd6df6c5762c067d1cfb673c05
limooh <limobrian290@gmail.com>
2019-09-02 08:09:44 +0300

    impotation from different file

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.conf.urls import url,include
+from django.contrib.auth import authenticate, login, logout
+from .forms import PostForm
+from django.conf.urls.static import static
+from .models import Profile, Image
+from django.core.mail import send_mail
+from annoying.decorators import ajax_reques
+from django.contrib.auth.models import User
+
+from django.contrib.auth.decorators import login_required
+from . import models
+
 
 # Create your views here.
 
